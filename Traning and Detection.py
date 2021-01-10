e5eafd2849f3f5984cc7d7fcfc6b0177616f40fb
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-11 01:52:37 +0500

    Practice 3

--- Traning and Detection.py
+++ Traning and Detection.py
@@ -1,35 +1,25 @@
 """
 Muhammad Ahsan Baig's Code
 """
+import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-# Adding salt & pepper noise to an image
-def salt_pepper(prob):
-      # Extract image dimensions
-      row, col = img_gs.shape
+# Declaring the output graph's size
+plt.figure(figsize=(16, 16))
 
-      # Declare salt & pepper noise ratio
-      s_vs_p = 0.5
-      output = np.copy(img_gs)
+# Convert image to grayscale
+img_gs = cv2.imread('cat.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('gs.jpg', img_gs)
 
-      # Apply salt noise on each pixel individually
-      num_salt = np.ceil(prob * img_gs.size * s_vs_p)
-      coords = [np.random.randint(0, i - 1, int(num_salt))
-            for i in img_gs.shape]
-      output[coords] = 1
+# Apply canny edge detector algorithm on the image to find edges
+edges = cv2.Canny(img_gs, 100,200)
 
-      # Apply pepper noise on each pixel individually
-      num_pepper = np.ceil(prob * img_gs.size * (1. - s_vs_p))
-      coords = [np.random.randint(0, i - 1, int(num_pepper))
-            for i in img_gs.shape]
-      output[coords] = 0
-      cv2_imshow(output)
+# Plot the original image against the edges
+plt.subplot(121), plt.imshow(img_gs)
+plt.title('Original Gray Scale Image')
+plt.subplot(122), plt.imshow(edges)
+plt.title('Edge Image')
 
-      return output
-
-# Call salt & pepper function with probability = 0.5
-# on the grayscale image of rose
-sp_05 = salt_pepper(0.5)
-
-# Store the resultant image as 'sp_05.jpg'
-cv2.imwrite('sp_05.jpg', sp_05)
+# Display the two images
+plt.show()
