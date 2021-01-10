cfb49077dad8682489304573edbe176e30dcfced
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-11 01:47:49 +0500

    Practice 2

--- Traning and Detection.py
+++ Traning and Detection.py
@@ -1,12 +1,35 @@
 """
 Muhammad Ahsan Baig's Code
 """
-from google.colab.patches import cv2_imshow
+import numpy as np
 
-blue, green, red = cv2.split(img) # Split the image into its channels
-img_gs = cv2.imread('rose.jpg', cv2.IMREAD_GRAYSCALE) # Convert image to grayscale
+# Adding salt & pepper noise to an image
+def salt_pepper(prob):
+      # Extract image dimensions
+      row, col = img_gs.shape
 
-cv2_imshow(red) # Display the red channel in the image
-cv2_imshow(blue) # Display the red channel in the image
-cv2_imshow(green) # Display the red channel in the image
-cv2_imshow(img_gs) # Display the grayscale version of image
+      # Declare salt & pepper noise ratio
+      s_vs_p = 0.5
+      output = np.copy(img_gs)
+
+      # Apply salt noise on each pixel individually
+      num_salt = np.ceil(prob * img_gs.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+            for i in img_gs.shape]
+      output[coords] = 1
+
+      # Apply pepper noise on each pixel individually
+      num_pepper = np.ceil(prob * img_gs.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+            for i in img_gs.shape]
+      output[coords] = 0
+      cv2_imshow(output)
+
+      return output
+
+# Call salt & pepper function with probability = 0.5
+# on the grayscale image of rose
+sp_05 = salt_pepper(0.5)
+
+# Store the resultant image as 'sp_05.jpg'
+cv2.imwrite('sp_05.jpg', sp_05)
