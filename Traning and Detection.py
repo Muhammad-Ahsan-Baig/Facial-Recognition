0f7ff1c9e749e72fdc386d970fabb7f81a9a8fc4
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-11 01:41:39 +0500

    Practice 1

--- Traning and Detection.py
+++ Traning and Detection.py
@@ -1,10 +1,12 @@
 """
 Muhammad Ahsan Baig's Code
 """
-import cv2
+from google.colab.patches import cv2_imshow
 
-img = cv2.imread('rose.jpg')
+blue, green, red = cv2.split(img) # Split the image into its channels
+img_gs = cv2.imread('rose.jpg', cv2.IMREAD_GRAYSCALE) # Convert image to grayscale
 
-print("Image Properties")
-print("- Number of Pixels: " + str(img.size))
-print("- Shape/Dimensions: " + str(img.shape))
+cv2_imshow(red) # Display the red channel in the image
+cv2_imshow(blue) # Display the red channel in the image
+cv2_imshow(green) # Display the red channel in the image
+cv2_imshow(img_gs) # Display the grayscale version of image
