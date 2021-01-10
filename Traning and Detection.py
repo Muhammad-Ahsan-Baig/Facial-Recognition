9bd5218686f774be4db7a64e36e6d052e0aefc3a
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-11 01:57:46 +0500

    Practice 4

--- Traning and Detection.py
+++ Traning and Detection.py
@@ -5,21 +5,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Declaring the output graph's size
-plt.figure(figsize=(16, 16))
-
-# Convert image to grayscale
-img_gs = cv2.imread('cat.jpg', cv2.IMREAD_GRAYSCALE)
-cv2.imwrite('gs.jpg', img_gs)
-
-# Apply canny edge detector algorithm on the image to find edges
-edges = cv2.Canny(img_gs, 100,200)
-
-# Plot the original image against the edges
-plt.subplot(121), plt.imshow(img_gs)
-plt.title('Original Gray Scale Image')
-plt.subplot(122), plt.imshow(edges)
-plt.title('Edge Image')
-
-# Display the two images
-plt.show()
+img = cv2.imread('watch.jpg',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
