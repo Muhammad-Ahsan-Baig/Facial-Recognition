ba2f584443caf590e638c71016da6f44cc54d342
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-11 01:38:23 +0500

    Practice

--- Traning and Detection.py
+++ Traning and Detection.py
@@ -1,4 +1,10 @@
 """
 Muhammad Ahsan Baig's Code
 """
-print("Hello")
+import cv2
+
+img = cv2.imread('rose.jpg')
+
+print("Image Properties")
+print("- Number of Pixels: " + str(img.size))
+print("- Shape/Dimensions: " + str(img.shape))
