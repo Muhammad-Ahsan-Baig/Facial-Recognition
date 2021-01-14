a86f64b8cfbeb9ce3274d84b9841e9f43b64a665
Muhammad-Ahsan-Baig <45787216+Muhammad-Ahsan-Baig@users.noreply.github.com>
2021-01-15 00:03:55 +0500

    new 5

--- Data Gathering.py
+++ Data Gathering.py
@@ -1,7 +1,5 @@
 """
 Usama Bin Nazim's Code
-"""
-
 import cv2
 import numpy as np
 
@@ -49,3 +47,26 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+"""
+import numpy as np
+import cv2
+
+face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap=cv2.VideoCapture(0)
+
+while True:
+	ret, img=cap.read()
+	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	faces=face_cascade.detectMultiScale(gray, 1.3, 5)
+	for(x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,255), 2)
+	cv2.imshow('FaceDetection',img)
+	k=cv2.waitKey(30) & 0xff
+	if k==27:
+		break
+	
+cap.release()
+cv2.destroyAllWindows()
+
